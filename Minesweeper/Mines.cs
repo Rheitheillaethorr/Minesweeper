edac292b461df0d5fd2e3eb554019d2354381558
Rheitheillaethor <minegocu3@gmail.com>
2021-12-03 19:17:51 +0100

    Add project files.

---
+++ Minesweeper/Mines.cs
@@ -0,0 +1,57 @@
+using System;
+public class Mines
+{
+    public bool[,] minesBoard { get; set; }
+    public int boardWidth { get; set; }
+    public int boardHeight { get; set; }
+    public int minesCount { get; set; }
+    public Mines(int width, int height, int Cmines)
+    {
+        boardWidth = width;
+        boardHeight = height;
+        minesCount = Cmines;
+        minesBoard = new bool[boardWidth, boardHeight];
+    }
+    public void SetMines()
+    {
+        if (minesCount > 0)
+        {
+            for (int i = 0; i < boardWidth; i++)
+            {
+                for (int j = 0; j < boardHeight; j++)
+                {
+                    Random rnd = new Random();
+                    int RandomNumber = rnd.Next(0, boardHeight);
+                    if (minesBoard[i, j] != true)
+                    {
+                        if (RandomNumber == 1 && minesCount > 0)
+                        {
+                            minesBoard[i, j] = true;
+                            minesCount--;
+                        }
+                        else
+                        {
+                            minesBoard[i, j] = false;
+                        }
+                    }
+                }
+            }
+            if (minesCount > 0)
+            {
+                SetMines();
+            }
+        }
+    }
+    public void CheckArray()
+    {
+        for (int i = 0; i < boardWidth; i++)
+        {
+            for (int j = 0; j < boardHeight; j++)
+            {
+                Console.Write("(" + i + "," + j + ") = " + minesBoard[i, j]);
+            }
+            Console.Write("\n");
+        }
+        Console.Write("mines to use = " + minesCount);
+    }
+}
---
+++ Minesweeper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Minesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 4;
+            int height = 4;
+            int Cmines = 5;
+            Mines MinesArrangement = new Mines(width, height, Cmines);
+            MinesArrangement.SetMines();
+            MinesArrangement.CheckArray();
+        }
+    }
+}
